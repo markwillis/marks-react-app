b306be588c6d7b027dd3cc1ff34599d4c9d9d88d
Mark <mark@webflow.com>
2020-01-05 21:30:27 +0000

    form handler for search input

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 
-function SearchInput() {
-  return (
-    <form>
-      <input
-        type="text"
-        className="search"
-        placeholder="Search for a joke..."
-      />
-      <button className="button">Amuse me!</button>
-    </form>
-  );
+class SearchInput extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      searchTerm: "",
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    event.preventDefault()
+    this.setState({searchTerm: event.target.value})
+    console.log(this.state.searchTerm)
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          className="search"
+          placeholder="Search for a joke..."
+          onChange={this.handleChange}
+          value={this.state.searchTerm}
+        />
+        <button className="button">Amuse me!</button>
+      </form>
+    );
+  }
 }
 
 export default SearchInput;
