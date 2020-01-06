2da2c525d5f86c5ccf4895e0cf960c0f55562159
Mark <mark@webflow.com>
2020-01-06 14:48:28 +0000

    moved fetch to App component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,42 @@ import React from "react";
 import Joke from "./components/Joke";
 import PageHeader from "./components/PageHeader";
 
-function App() {
-  return (
-    <div className="App">
-      <PageHeader />
-      <div className="joke-wrapper">
-        <Joke />
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      jokeData: {
+        results: []
+      }
+    }
+  }
+
+  async componentDidMount() {
+    const url = 'https://icanhazdadjoke.com/search?term="cat"';
+    const fetchUrl = fetch(url, {
+      method: "GET",
+      headers: { accept: "application/json" }
+    });
+    const response = await fetchUrl;
+    const myData = await response.json();
+    this.setState({ jokeData: { results: myData.results } });
+  }
+
+  // This needs rendering inside a map function, then rendering in the render as <div>{jokeList}</div>
+  // <Joke key="[ENTER THE KEY HERE]" joke={this.state}/>
+  render() {
+    const jokeList = this.state.jokeData.results.map(item => (
+      <Joke key={item.id} joke={item.joke} />
+    ))
+    return (
+      <div className="App">
+        <PageHeader />
+        <div className="joke-wrapper">
+          {jokeList}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,38 +1,12 @@
 import React from "react";
 
-class Joke extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      jokeData: {
-        results: [
-          {
-            joke: "Loading..."
-          }
-        ]
-      }
-    };
-  }
 
-  async componentDidMount() {
-    const url = 'https://icanhazdadjoke.com/search?term="cat"';
-    const fetchUrl = fetch(url, {
-      method: "GET",
-      headers: { accept: "application/json" }
-    });
-    const response = await fetchUrl;
-    const myData = await response.json();
-    this.setState({ jokeData: { results: myData.results } });
-  }
-
-  render() {
-    const jokes = this.state.jokeData.results.map(item => (
-      <div className="joke">
-        <p>{item.joke}</p>
-      </div>
-    ));
-    return <React.Fragment>{jokes}</React.Fragment>;
-  }
+function Joke(props) {
+  return (
+    <div className="joke">
+      <h3>{props.joke}</h3>
+    </div>
+  )
 }
 
 export default Joke;
